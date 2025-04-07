c1e72e5477f9ffb5c593d390bbabcef10413dc63
Ivan <120404084+altervee@users.noreply.github.com>
2025-04-07 10:35:46 +0200

    Remove merge conflict markers and clean up code in multiple files

--- config/database.php
+++ config/database.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 class Database 
 {
@@ -25,32 +24,4 @@ exit;
     }
     }
 }
-=======
-<?php
-class Database 
-{
-    private $hostname ="localhost";
-    private $database ="tienda_online";
-    private $username ="sa";
-    private $password ="sa";
-    private $charset ="utf8";
-    
-    function conectar()
-    {
-        try{
-    $conexion = "mysql:host=" . $this->hostname ."; dbname=" . $this->database . "; 
-    charset=". $this->charset;
-    $options = [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_EMULATE_PREPARES => false
-    ];
-    $pdo = new PDO($conexion, $this->username,  $this->password, $options); 
-    return $pdo;
-    }catch(PDOException $e){
-echo 'Error conexion: ' . $e->getMessage();
-exit;
-    }
-    }
-}
->>>>>>> aa635ac89f4ca6c2a74ce5a3c47f6b80dcc738d0
 ?>
